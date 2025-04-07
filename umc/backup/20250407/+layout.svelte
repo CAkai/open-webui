c4e1b3e91263a3746c5ee3664350556e57d3a048
Arvin Yang <sugky7302@gmail.com>
2025-04-07 11:31:55 +0800

    feat: merge 0.6.1

--- umc/backup/20250407/+layout.svelte
+++ umc/backup/20250407/+layout.svelte
@@ -266,8 +266,7 @@
 	};
 
 	const executeTool = async (data, cb) => {
-		const toolServer = $settings?.toolServers?.find((server) => server.url === data.server?.url);
-		const toolServerData = $toolServers?.find((server) => server.url === data.server?.url);
+		const toolServer = $toolServers?.find((server) => server.url === data.server?.url);
 
 		console.log('executeTool', data, toolServer);
 
@@ -277,7 +276,7 @@
 				toolServer.url,
 				data?.name,
 				data?.params,
-				toolServerData
+				toolServer
 			);
 
 			console.log('executeToolServer', res);
