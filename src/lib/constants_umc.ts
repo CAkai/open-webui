12915c429e471281e8555a4cd3159f59a06bcc79
Arvin Yang <sugky7302@gmail.com>
2025-04-07 14:39:27 +0800

    env: change ICLOUD_API_BASE_URL

--- src/lib/constants_umc.ts
+++ src/lib/constants_umc.ts
@@ -1,3 +1,3 @@
 // 這個檔案會先被 envsubst 覆寫，然後再寫入 src/lib/constants_umc.ts
-export const ICLOUD_API_BASE_URL = "http://192.168.1.165:3003";
+export const ICLOUD_API_BASE_URL = "http://192.168.1.113:3003";
 export const UMC_TOKEN_COOKIE_KEY = "umc-camera-access_token";
